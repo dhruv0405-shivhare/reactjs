178be7f79976a4832f5061b717db6677bf88fdcc
Dhruv <dshivhare661@gmail.com>
2024-12-19 12:34:37 +0530

    states

---
+++ states_hooks/Show.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function Show(){
+    let[h,setH] = useState(false)
+    let p = ''
+
+      if(h){
+        p=<h1>The time is now</h1>
+      }
+      else{
+        p=''
+      }
+    
+    return(
+  
+      <>
+          <button onClick={()=>setH(!h)}>Show/Hide</button>
+{p}
+    </>
+  )
+}
+
+export default Show
---
+++ states_hooks/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Show from "../Show";
+function App() {
+   let [col,setcol] = useState('yellow')
+
+  function blue(){
+    setcol('blue')
+  }
+  function red(){
+    setcol('red')
+  }
+  function black(){
+    setcol('black')
+  }
+  function orange(){
+    setcol('orange')
+  }
+  return(
+
+    <>
+    <div style={{height:"50vh" , width:"50vw" , marginLeft:"20px" , marginBottom:"20px" , backgroundColor: col}}>
+      
+        
+
+
+    </div>
+
+    <button onClick={red} style={{marginLeft:"20px"}}>red</button>
+<button onClick={blue} style={{marginLeft:"20px"}}>blue</button>
+<button onClick={black} style={{marginLeft:"20px"}}>black</button>
+<button onClick={orange} style={{marginLeft:"20px" , marginRight:"20px"}}>orange</button>
+    <Show/>
+    </>
+  )
+}
+
+export default App
